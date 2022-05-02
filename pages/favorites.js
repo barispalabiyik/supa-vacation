88526f2d4158d2051595b3167c628a43ce26a1a5
BARIŞ PALABIYIK <baris.palabiyik@ticketnetwork.com>
2022-05-03 00:13:40 +0300

    favoritespage

---
+++ pages/favorites.js
@@ -0,0 +1,32 @@
+import { getSession } from 'next-auth/react';
+import Layout from '@/components/Layout';
+import Grid from '@/components/Grid';
+import { prisma } from '@/lib/prisma';
+
+export async function getServerSideProps() {
+    // Get all homes
+    const homes = await prisma.home.findMany();
+    // Pass the data to the Home page
+    return {
+      props: {
+        homes: JSON.parse(JSON.stringify(homes)),
+      },
+    };
+  }
+  
+
+const Favorites = ({ homes = [] }) => {
+  return (
+    <Layout>
+      <h1 className="text-xl font-medium text-gray-800">Your listings</h1>
+      <p className="text-gray-500">
+        Manage your homes and update your listings
+      </p>
+      <div className="mt-8">
+        <Grid homes={homes} />
+      </div>
+    </Layout>
+  );
+};
+
+export default Favorites;
